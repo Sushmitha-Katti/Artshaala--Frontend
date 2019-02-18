0f19837f1d8c683f0ff1eb6341a9d719cf780224
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-02-18 23:37:26 +0530

    add images dynamically

--- components/page/Home/Servicecard.js
+++ components/page/Home/Servicecard.js
@@ -2,22 +2,24 @@ import React, { Component } from "react";
 import Card from "./styles";
 import Link from "next/link";
 
-import musicstore from "./musicstore.png";
-
 class ServiceCard extends Component {
   render() {
     return (
       <Card>
         <div className="cardhover">
-          <div className="image">
-            <img alt="IMAGE" src={musicstore} />
-          </div>
+          <img
+            className="image"
+            alt="IMAGE"
+            src={Object.values(this.props.Cardcontent.img).toString()}
+          />
+          <div className="imageoverlay" />
+          <div className="hoveroverlay" />
           <div className="title">
             <h4>{this.props.Cardcontent.title}</h4>
           </div>
           <Link href="/">
-            <a>
-              <h3 className="button">Explore</h3>
+            <a className="button">
+              <b>Explore</b>
             </a>
           </Link>
         </div>
