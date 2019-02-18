90035ee2df6542dc76e2eae304be5a3eb70903ee
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-02-18 23:38:30 +0530

    add media queries for cards

--- components/page/Home/Servicecards.js
+++ components/page/Home/Servicecards.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 import ServiceCard from "./Servicecard";
 import styled, { keyframes } from "styled-components";
+import musicstore from "./musicstore.png";
+import sale from "./sale.jpg";
+import service from "./Servicea.jpg";
+import rental from "./rental.jpg";
 
 const CardWrapper = styled.div`
   display: flex;
   margin: 3rem;
   justify-content: space-between;
-`;
+  flex-wrap: wrap;
 
-import musicstore from "./musicstore.png";
+  @media only screen and (max-width: 768px) {
+    margin: 0.5rem;
+  }
+`;
 
 class ServiceCards extends Component {
   render() {
     let Cards = [
-      { title: "Music Store", img: " " },
-      { title: "Service & Repairs", img: " " },
-      { title: "Rentals", img: " " },
-      { title: "Mega Sale", img: " " }
+      { title: "Music Store", img: { musicstore } },
+      { title: "Service & Repairs", img: { service } },
+      { title: "Rentals", img: { rental } },
+      { title: "Mega Sale", img: { sale } }
     ].map(card => {
       card.key = `{card.title}`;
       return card;
