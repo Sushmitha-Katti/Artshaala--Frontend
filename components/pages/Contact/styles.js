e6f764af908f9da562606df0daa83854b31f3058
Ankit Chauhan <ankit.chauhan@unifize.com>
2019-02-19 20:06:49 +0530

    Fix issues with branch

--- components/pages/Contact/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Heading = style.div`
-    font-size: 2rem;
-`;
-
-export { Heading };
--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-module.exports = {
+const withImages = require("next-images");
+module.exports = withImages({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
-    }
+      fs: "empty"
+    };
 
-    return config
+    return config;
   }
-}
+});
