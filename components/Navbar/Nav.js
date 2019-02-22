a864a962414b83d26106023bf7d21172e1fed1f4
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-02-23 01:07:17 +0530

    add header

--- components/Navbar/Nav.js
+++ components/Navbar/Nav.js
@@ -2,37 +2,45 @@ import React, { Component } from "react";
 import Navbar from "./index.js";
 import NavWrapper from "./styles.js";
 import Link from "next/link";
+import logo from "./logo.png";
 
 class Nav extends Component {
   render() {
-    console.log("Hello");
-    console.log("this.props");
     return (
       <NavWrapper>
-        <div className="nav-btn">
-          <label htmlFor="nav-check">
-            <span />
-            <span />
-            <span />
-          </label>
+        <div className="Header">
+          <img className="image" alt="Logo" src={logo} />
+          <div className="contact">
+            <i class="fa fa-phone fa-lg" aria-hidden="true" />
+            (+91) 73380 40655
+          </div>
         </div>
-        <input type="checkbox" id="nav-check" />
+        <div className="Navbar">
+          <div className="nav-btn">
+            <label htmlFor="nav-check">
+              <span />
+              <span />
+              <span />
+            </label>
+          </div>
+          <input type="checkbox" id="nav-check" />
 
-        <ul className="nav-links">
-          {this.props.links.map(({ key, href, label }) => (
-            <li key={key}>
-              <Link href={href}>
-                <a>{label}</a>
-              </Link>
+          <ul className="nav-links">
+            {this.props.links.map(({ key, href, label }) => (
+              <li key={key}>
+                <Link href={href}>
+                  <a>{label}</a>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <form action="/">
+                <input type="text" placeholder="Search.." name="search" />
+                <i className="fa fa-search" />
+              </form>
             </li>
-          ))}
-          <li>
-            <form action="/">
-              <input type="text" placeholder="Search.." name="search" />
-              <i className="fa fa-search" />
-            </form>
-          </li>
-        </ul>
+          </ul>
+        </div>
       </NavWrapper>
     );
   }
