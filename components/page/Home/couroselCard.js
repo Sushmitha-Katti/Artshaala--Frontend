5ab0c884ff9f458e837e88b72d767d9ba40f430d
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-03-08 21:22:07 +0530

    make ratings dynamic

--- components/page/Home/couroselCard.js
+++ components/page/Home/couroselCard.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import Link from "next/link";
-import styled from "styled-components";
-import guitar from "./guitar.jpg";
 import { CouroselWrapper } from "./styles";
+
+/*          for displaying the ratings            */
+let ratings = rating => {
+  let toatal_ratings = [];
+
+  for (let j = 0; j < rating; j++) {
+    toatal_ratings.push(<i class="fa fa-star" aria-hidden="true" />);
+  }
+  return toatal_ratings;
+};
 
 class CouroselCard extends Component {
   render() {
@@ -11,19 +19,17 @@ class CouroselCard extends Component {
         <div className="card">
           <Link href="/">
             <div className="cardimg">
-              <img className="cardimg" alt="IMAGE" src={guitar} />
+              <img
+                className="cardimg"
+                alt="IMAGE"
+                src={Object.values(this.props.property.img).toString()}
+              />
               <div className="name">
                 {this.props.property.name}
                 {this.props.property.id}
               </div>
               <div className="rating">
-                <span>
-                  <i class="fa fa-star" aria-hidden="true" />
-                  <i class="fa fa-star" aria-hidden="true" />
-                  <i class="fa fa-star" aria-hidden="true" />
-                  <i class="fa fa-star" aria-hidden="true" />
-                  <i class="fa fa-star" aria-hidden="true" />
-                </span>
+                <span>{ratings(this.props.property.rating)}</span>
                 <span className="amount">
                   INR {this.props.property.amount}/-
                 </span>
