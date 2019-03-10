7f4070ac55334aa940f5d6d106f72030772a9fa7
sharushri <36913871+sharushri@users.noreply.github.com>
2019-03-10 23:18:52 +0530

    Update index.js

--- components/page/Contact/index.js
+++ components/page/Contact/index.js
@@ -80,7 +80,7 @@ class Contact extends Component {
 
               <h2>Email</h2>
               <h4>
-                <a href="mail-to:artshaalamusicstore@gmail.com">
+                <a href="mailto:artshaalamusicstore@gmail.com">
                   artshaalamusicstore@gmail.com
                 </a>
               </h4>
