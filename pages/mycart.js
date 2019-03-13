b8e9b550ef23ddfd8ed69341f0bba2974649c1a1
Ankit Chauhan <ankit.chauhan@unifize.com>
2019-03-14 01:03:12 +0530

    Add Nav on the cart page

--- pages/mycart.js
+++ pages/mycart.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Cart from "../components/page/cart/index";
 import Head from "../components/head";
+import Nav from "../components/Navbar";
 
 const page = () => (
   <div>
     <Head title="Cart" />
+    <Nav />
     <Cart />
   </div>
 );
