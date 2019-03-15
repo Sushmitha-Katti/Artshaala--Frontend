bba0e320df38ee0c42340086c3debb689114592c
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-03-15 16:15:21 +0530

    resolve errors

--- components/page/Rental/styles.js
+++ components/page/Rental/styles.js
@@ -1,8 +1,5 @@
-
 import sc from "styled-components";
 import img from "./images/gutar.png";
-import sc from "styled-components";
-import img from './images/gutar.png';
 
 const Body = sc.body`
  box-sizing: border-box;
@@ -353,9 +350,6 @@ grid-area:footer;
                 
 }
 
- `;
-
-export default Body;
  `;
 
 export default Body;
