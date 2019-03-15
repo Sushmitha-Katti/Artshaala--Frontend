00de61103b748dd7b0d0deeb289cbdfbe9b5faec
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-03-15 16:12:47 +0530

    Remove duplicate entries

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
-import NavWrapper from "./styles.js";
+//import NavWrapper from "./styles.js";
 
 const Nav = () => (
   <NavWrapper>
