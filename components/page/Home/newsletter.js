9756018e467df10ae680446e5fd26062bf00a6d6
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-03-17 01:46:55 +0530

    add styled components

--- components/page/Home/newsletter.js
+++ components/page/Home/newsletter.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 import styled, { keyframes } from "styled-components";
 import AutoCards from "./autocards";
-import autoguitar from "./autoguitar.png";
+import autoguitar from "./autoguitar.jpg";
 
+/****************************************   Styled Components   **************************************** */
 const D = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+const Slider = styled.div`
+  font-family: "Montserrat", sans-serif;
+  width: 660px;
+  height: 200px;
+  color: ${props => props.color};
+  text-align: center;
+  padding: 8rem 0rem;
+  background: ${props => props.background};
+  position: relative;
+
+  .back {
+    width: 100%;
+    height: 100%;
+    z-index: 5;
+  }
+  .imageoverlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0.8;
+
+    background-color: #000000;
+  }
+`;
 
 const Newsletter = styled.div.attrs({})`
   font-family: "Montserrat", sans-serif;
-  width: 560px;
+  width: 580px;
   height: 200px;
   color: ${props => props.color};
   text-align: center;
@@ -36,6 +65,8 @@ const Newsletter = styled.div.attrs({})`
     border-radius: 50%;
   }
 `;
+
+/********************************************************************************** */
 class NewsLetter extends Component {
   render() {
     return (
@@ -60,9 +91,11 @@ class NewsLetter extends Component {
             />
           </form>
         </Newsletter>
-        <Newsletter background="#505050" color="white">
+        <Slider color="white">
+          <img className="back" src={autoguitar} alt="guitar" />
+          <div className="imageoverlay" />
           <AutoCards />
-        </Newsletter>
+        </Slider>
       </D>
     );
   }
