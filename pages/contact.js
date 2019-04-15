412d017f7b40eee905ce9616448b5a5e780396ef
Sushmitha M Katti <sushmitha.katti.19@gmail.com>
2019-04-15 17:03:34 +0530

    add navbar to all pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Contact from "../components/page/Contact/index";
 import Head from "../components/head";
+import Nav from "../components/Navbar";
 
 const Page = () => (
   <div>
     <Head title="Contact" />
+    <Nav/>
     <Contact />
   </div>
 );
--- pages/rental.js
+++ pages/rental.js
@@ -1,9 +1,20 @@
 
 import React from 'react'
 import Rental from '../components/page/Rental/index';
+import Nav from "../components/Navbar";
+import Head from "../components/head";
 
 
 
-export default (params) => <Rental {...params} />
 
 
+const rental = () => (
+    <div>
+      <Head title="Rental" />
+      <Nav />
+      <Rental  />
+
+    </div>
+  );
+  
+  export default rental;
