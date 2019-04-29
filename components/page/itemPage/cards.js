717be3960c68a136199ed8e1f5b5e14cc2e51104
Sushmitha M Katti <sushmitha.katti.19@gmail.com>
2019-04-29 22:22:01 +0530

    add deleteitem and updateitem

--- components/page/itemPage/cards.js
+++ components/page/itemPage/cards.js
@@ -2,6 +2,34 @@ import React, { Component } from "react";
 import Cardstyle from "./cardStyle";
 import Link from "next/link";
 
+import {CURRENT_USER_QUERY} from "../../test/User"
+import { Query } from "react-apollo";
+import DeleteItem from './deleteItem';
+import styled from "styled-components";
+
+const Update = styled.div`
+.updatebtn{
+  text-decoration:none;
+  color:black;
+  font-family: Arial;
+  padding:5px 12px;
+  background:#DDDDDD;
+  font-style: normal;
+    font-variant-ligatures: normal;
+    font-variant-caps: normal;
+    font-variant-numeric: normal;
+    font-variant-east-asian: normal;
+    font-weight: 400;
+    font-stretch: normal;
+    font-size: 13.3333px;
+    line-height: normal;
+    margin-left: 10px;
+}
+`
+;
+
+
+
 
 
 
@@ -9,6 +37,8 @@ import Link from "next/link";
 class Cards extends Component {
   render() {
     return (
+      <div>
+      
       <Link href={{pathname:'/product', query:{id:this.props.Cardcontent.id}}}>
 
 
@@ -50,9 +80,36 @@ class Cards extends Component {
               </div>
             </div>
           </div>
+         
         </div>
       </Cardstyle>
       </Link>
+      <Query query={CURRENT_USER_QUERY}>
+      {({ data }) => {
+      let { me } = data;
+     
+      
+      return (
+        <Update>
+        {me.permissions.includes("ADMIN") && <div>
+       <DeleteItem id={this.props.Cardcontent.id}/> 
+       
+       <Link href ={{
+              pathname: '/update',
+              query: { id: this.props.Cardcontent.id },
+            }}><a className = "updatebtn">Update</a></Link>
+          </div>
+          
+          
+        } 
+     
+       </Update>
+        
+      );
+      }}
+
+    </Query>
+       </div>
     );
   }
 }
---
+++ components/page/itemPage/deleteItem.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { ALL_ITEMS_QUERY } from './Items';
+import {Form} from "../../test/signuppage.js";
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+
+class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // manually update the cache on the client, so it matches the server
+    // 1. Read the cache for the items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log(data, payload);
+    // 2. Filter the deleted itemout of the page
+    data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
+    // 3. Put the items back!
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button style = {{border:"black",paddingTop:5+"px",paddingBottom:5+"px",paddingRight:12+"px",paddingLeft:12+"px" }}
+            onClick={() => {
+              if (confirm('Are you sure you want to delete this item?')) {
+                deleteItem().catch(err => {
+                  alert(err.message);
+                });
+              }
+            }}
+          >
+            Delete
+          </button>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+export default DeleteItem;
