f7ab266e8d89c192ab9afbb66fc3f048561ed015
Sushmitha M Katti <sushmitha.katti.19@gmail.com>
2019-05-01 09:13:01 +0530

    develop admin page features

--- pages/admin.js
+++ pages/admin.js
@@ -1,14 +1,14 @@
 import Common from "../components/page/common/common";
 import Head from "../components/head";
-
-
-
+import Notification  from "../components/Admin/notification";
+import {CURRENT_USER_QUERY} from "../components/test/signuppage";
 import Navtest from "../components/test/navtest";
 
 const Admin = () => (
     <div>
       <Head title="Admin" />
       <Navtest />
+      <Notification/>
     
     </div>
   );
