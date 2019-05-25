74e832bf4b9e71bacb259126fc6deaf4e56d27b2
Sushmitha M Katti <sushmitha.katti.19@gmail.com>
2019-05-25 18:37:27 +0530

    add dropdown for store

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -4,6 +4,24 @@ import styled from "styled-components";
 import { Mutation } from 'react-apollo';
 import User from '../../components/test/User';
 import Nav from "./Nav.js";
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+
+
+
+const CATEGORY_AND_BRAND_QUERY = gql`
+  query  CATEGORY_AND_BRAND_QUERY {
+    items {
+      category
+      type      
+     
+      
+    }
+  }
+  
+`;
+
 
 let commonlinks = [
    { href: "/", label: "Home" },
@@ -60,13 +78,26 @@ let isauth = true;
 let cartcount = 0;
 let admin = false;
 
+
+
+
+
+
 const Navbar = () => (
   <User>
     {({ data }) => {
+     
+      
       const me = data ? data.me : null
+      
+     
+
+      
 
      //if there is a user
       if(me  && !me.permissions.includes('ADMIN') ){
+        
+
          links = commonlinks.concat(authlinks)
          isauth = true;
          cartcount = me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
@@ -74,6 +105,9 @@ const Navbar = () => (
       }
       //if he is a admin
       else if(me && me.permissions.includes('ADMIN')){
+      
+        
+     
         links = commonlinks.concat(adminlinks);
         admin = true;
         isauth = true;
@@ -85,8 +119,29 @@ const Navbar = () => (
       }
       
    
-      return(<Nav links={links} isauth = {isauth} cartcount = {cartcount} admin = {admin}/>)
+      return(
+
+        <Query                           //Item Query
+        query={CATEGORY_AND_BRAND_QUERY}
+        >
+        {({ error, loading, data }) => {
+        if (error) return <Error error={error} />;
+        if (loading) return <p>Loading...</p>;
+       
+        const items = data.items;
+         let distcategory  = [... new Set(items.map(item => item.category))]
+        let catandtype = {};
+        
+        distcategory.map(category => catandtype[category] = [...new Set(items.map(item => item.category===category && item.type))].filter(itemtype => itemtype!=false) );
+        console.log(catandtype)
+        return (
+      <Nav links={links} isauth = {isauth} cartcount = {cartcount} admin = {admin} catandtype = {catandtype}/>)
     }}
+    </Query>
+    )
+    }}
+   
+
   </User>
 );
 
@@ -110,3 +165,4 @@ const Navbar = () => (
 // }
 
 export default Navbar;
+export {CATEGORY_AND_BRAND_QUERY};
