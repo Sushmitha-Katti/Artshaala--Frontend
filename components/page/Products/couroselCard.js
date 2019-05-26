af3d0fe50845a05510758e9636199131cfdc35ff
mshilpa <goldenflowers21@gmail.com>
2019-05-26 23:38:28 +0530

    Related products displaying

--- components/page/Products/couroselCard.js
+++ components/page/Products/couroselCard.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 import { CouroselWrapper } from "./styles";
 
 /*          for displaying the ratings            */
-let ratings = rating => {
-  let toatal_ratings = [];
+// let ratings = rating => {
+//   let toatal_ratings = [];
 
-  for (let j = 0; j < rating; j++) {
-    toatal_ratings.push(<i class="fa fa-star individualstar" aria-hidden="true" />);
-  }
-  return toatal_ratings;
-};
+//   for (let j = 0; j < rating; j++) {
+//     toatal_ratings.push(<i class="fa fa-star individualstar" aria-hidden="true" />);
+//   }
+//   return toatal_ratings;
+// };
 
 class CouroselCard extends Component {
   render() {
@@ -22,16 +22,16 @@ class CouroselCard extends Component {
               <img
                 className="cardimg"
                 alt="IMAGE"
-                src={Object.values(this.props.property.img).toString()}
+                src={this.props.property.images[0]}
               />
               <div className="name">
-                {this.props.property.name}
+                {this.props.property.title}
                 {this.props.property.id}
               </div>
               <div className="rating">
                 {/* <div>{ratings(this.props.property.rating)}</div> */}
                 <div className="amount">
-                  INR {this.props.property.amount}/-
+                  INR {this.props.property.price}/-
                 </div>
               </div>
             </div>
@@ -43,3 +43,35 @@ class CouroselCard extends Component {
 }
 
 export default CouroselCard;
+
+// class CouroselCard extends Component {
+//   render() {
+//     return (
+//       <CouroselWrapper>
+//         <div className="card">
+//           <Link href="/">
+//             <div className="cardimg">
+//               <img
+//                 className="cardimg"
+//                 alt="IMAGE"
+//                 src={Object.values(this.props.property.img).toString()}
+//               />
+//               <div className="name">
+//                 {this.props.property.name}
+//                 {this.props.property.id}
+//               </div>
+//               <div className="rating">
+//                 {/* <div>{ratings(this.props.property.rating)}</div> */}
+//                 <div className="amount">
+//                   INR {this.props.property.amount}/-
+//                 </div>
+//               </div>
+//             </div>
+//           </Link>
+//         </div>
+//       </CouroselWrapper>
+//     );
+//   }
+// }
+
+// export default CouroselCard;
